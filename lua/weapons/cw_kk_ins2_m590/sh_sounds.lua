7b238ba9796a5e195011a2039bf6944d8553be28
Isakov Dmitry <dima.isakov03@mail.ru>
2023-06-20 14:33:15 +0800

    Update sh_sounds.lua

--- lua/weapons/cw_kk_ins2_m590/sh_sounds.lua
+++ lua/weapons/cw_kk_ins2_m590/sh_sounds.lua
@@ -1,7 +1,7 @@
 CustomizableWeaponry:addFireSound("CW_KK_INS2_M590_FIRE", "weapons/m590/m590_fp.wav", 1, 105, CHAN_WEAPON)
 CustomizableWeaponry:addFireSound("CW_KK_INS2_M590_FIRE_SUPPRESSED", "weapons/m590/m590_suppressed_fp.wav", 1, 75, CHAN_WEAPON)
 
-CustomizableWeaponry:addReloadSound("CW_KK_INS2_M590_EMPTY", "weapons/m590/handling/m590_empty.wav"), 1, 100, CHAN_WEAPON)
+CustomizableWeaponry:addReloadSound("CW_KK_INS2_M590_EMPTY", "weapons/m590/handling/m590_empty.wav", 1, 100, CHAN_WEAPON)
 CustomizableWeaponry:addReloadSound("CW_KK_INS2_M590_PUMPBACK", "weapons/m590/handling/m590_pumpback.wav", 1, 100, CHAN_WEAPON)
 CustomizableWeaponry:addReloadSound("CW_KK_INS2_M590_PUMPFORWARD", "weapons/m590/handling/m590_pumpforward.wav", 1, 100, CHAN_WEAPON)
 CustomizableWeaponry:addReloadSound("CW_KK_INS2_M590_SHELLINSERT", "weapons/m590/handling/m590_shell_insert_1.wav", "weapons/m590/handling/m590_shell_insert_2.wav", "weapons/m590/handling/m590_shell_insert_3.wav", 1, 100, CHAN_WEAPON)
